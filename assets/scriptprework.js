595db68f3aea23a5c4be077aefbd51f5da77e6c3
øLu <tobiakinyemi21@gmail.com>
2022-11-10 19:52:29 -0600

    adding JavaScript

---
+++ assets/scriptprework.js
@@ -0,0 +1,36 @@
+/*var topic = "JavaScript";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}*/
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+}
+/*
+    used to study 
+
+function lCat(cat) {
+    return cat.startsWith('L');
+  }
+  
+  const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
+  
+  const filtered = cats.filter(lCat);
+  
+  console.log(filtered);
+
+    the top code is usuually written as such 
+
+  const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
+
+  const filtered = cats.filter((cat) => cat.startsWith('L'));
+console.log(filtered); */
